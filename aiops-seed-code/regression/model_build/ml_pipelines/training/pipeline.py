3a0f5f39f6400a30b7116c47c2f13eaf0e9784fe
vaibhavsabharwal <77911293+vaibhavsabharwal@users.noreply.github.com>
2025-07-24 16:41:45 -0400

    Glue catalog and tables changes

--- aiops-seed-code/regression/model_build/ml_pipelines/training/pipeline.py
+++ aiops-seed-code/regression/model_build/ml_pipelines/training/pipeline.py
@@ -1,110 +1,14 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# SPDX-License-Identifier: MIT-0
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this
-# software and associated documentation files (the "Software"), to deal in the Software
-# without restriction, including without limitation the rights to use, copy, modify,
-# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-"""Example workflow pipeline script for abalone pipeline.
-
-                                               . -RegisterModel
-                                              .
-    Process-> Train -> Evaluate -> Condition .
-                                              .
-                                               . -(stop)
-
-Implements a get_pipeline(**kwargs) method.
-"""
-import os
-
-import boto3
-import logging
-import sagemaker
-import sagemaker.session
-
-from sagemaker.estimator import Estimator
-from sagemaker.inputs import TrainingInput
-from sagemaker.model_metrics import (
-    MetricsSource,
-    ModelMetrics,
-)
-from sagemaker.processing import (
-    ProcessingInput,
-    ProcessingOutput,
-    ScriptProcessor,
-)
-from sagemaker.sklearn.processing import SKLearnProcessor
-from sagemaker.workflow.conditions import ConditionLessThanOrEqualTo
-from sagemaker.workflow.condition_step import (
-    ConditionStep,
-)
-from sagemaker.workflow.functions import (
-    JsonGet,
-)
-from sagemaker.workflow.parameters import (
-    ParameterInteger,
-    ParameterString,
-)
-from sagemaker.workflow.pipeline import Pipeline
-from sagemaker.workflow.properties import PropertyFile
-from sagemaker.workflow.steps import (
-    ProcessingStep,
-    TrainingStep,
-)
-from sagemaker.workflow.step_collections import RegisterModel
-
-from botocore.exceptions import ClientError
-from sagemaker.network import NetworkConfig
-
-# BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-
-logger = logging.getLogger(__name__)
-
-
-def get_session(region, default_bucket):
-    """Gets the sagemaker session based on the region.
-
-    Args:
-        region: the aws region to start the session
-        default_bucket: the bucket to use for storing the artifacts
-
-    Returns:
-        `sagemaker.session.Session instance
-    """
-
-    boto_session = boto3.Session(region_name=region)
-
-    sagemaker_client = boto_session.client("sagemaker")
-    runtime_client = boto_session.client("sagemaker-runtime")
-    session = sagemaker.session.Session(
-        boto_session=boto_session,
-        sagemaker_client=sagemaker_client,
-        sagemaker_runtime_client=runtime_client,
-        default_bucket=default_bucket,
-    )
-
-    return session
-
-
 def get_pipeline(
     region,
     role=None,
     default_bucket=None,
-    bucket_kms_id=None,
     model_package_group_name="AbalonePackageGroup",
     pipeline_name="AbalonePipeline",
     base_job_prefix="Abalone",
-    project_id="SageMakerProjectId",
+    bucket_kms_id=None,
+    sagemaker_session=None,
+    glue_database_name=None,
+    glue_table_name=None,
 ):
     """Gets a SageMaker ML Pipeline instance working with on abalone data.
 
@@ -116,85 +20,116 @@ def get_pipeline(
     Returns:
         an instance of a pipeline
     """
-
-    sagemaker_session = get_session(region, default_bucket)
-    if role is None:
-        role = sagemaker.session.get_execution_role(sagemaker_session)
-
-    # parameters for pipeline execution
-    processing_instance_count = ParameterInteger(name="ProcessingInstanceCount", default_value=1)
-    processing_instance_type = ParameterString(name="ProcessingInstanceType", default_value="ml.m5.xlarge")
-    training_instance_type = ParameterString(name="TrainingInstanceType", default_value="ml.m5.xlarge")
-    inference_instance_type = ParameterString(name="InferenceInstanceType", default_value="ml.m5.xlarge")
-    model_approval_status = ParameterString(name="ModelApprovalStatus", default_value="PendingManualApproval")
-    input_data = ParameterString(
-        name="InputDataUrl",
-        default_value=f"s3://{default_bucket}/ml_pipelines/data/abalone-dataset.csv",
+    import boto3
+    import sagemaker
+    import sagemaker.session
+    
+    from sagemaker.estimator import Estimator
+    from sagemaker.inputs import TrainingInput
+    from sagemaker.model_metrics import (
+        MetricsSource,
+        ModelMetrics,
     )
-    processing_image_name = "sagemaker-{0}-processingimagebuild".format(project_id)
-    training_image_name = "sagemaker-{0}-trainingimagebuild".format(project_id)
-    inference_image_name = "sagemaker-{0}-inferenceimagebuild".format(project_id)
-
-    # network_config = NetworkConfig(
-    #     enable_network_isolation=True,
-    #     security_group_ids=security_group_ids,
-    #     subnets=subnets,
-    #     encrypt_inter_container_traffic=True,
-    # )
-
-    # processing step for feature engineering
-    try:
-        processing_image_uri = sagemaker_session.sagemaker_client.describe_image_version(
-            ImageName=processing_image_name
-        )["ContainerImage"]
-    except (sagemaker_session.sagemaker_client.exceptions.ResourceNotFound):
-        processing_image_uri = sagemaker.image_uris.retrieve(
-            framework="xgboost",
+    from sagemaker.processing import (
+        ProcessingInput,
+        ProcessingOutput,
+        ScriptProcessor,
+    )
+    from sagemaker.sklearn.processing import SKLearnProcessor
+    from sagemaker.workflow.conditions import ConditionLessThanOrEqualTo
+    from sagemaker.workflow.condition_step import (
+        ConditionStep,
+    )
+    from sagemaker.workflow.functions import (
+        JsonGet,
+    )
+    from sagemaker.workflow.parameters import (
+        ParameterInteger,
+        ParameterString,
+    )
+    from sagemaker.workflow.pipeline import Pipeline
+    from sagemaker.workflow.properties import PropertyFile
+    from sagemaker.workflow.steps import (
+        ProcessingStep,
+        TrainingStep,
+    )
+    from sagemaker.workflow.step_collections import RegisterModel
+    
+    # Parameters for pipeline execution
+    processing_instance_type = ParameterString(
+        name="ProcessingInstanceType", default_value="ml.m5.xlarge"
+    )
+    processing_instance_count = ParameterInteger(
+        name="ProcessingInstanceCount", default_value=1
+    )
+    training_instance_type = ParameterString(
+        name="TrainingInstanceType", default_value="ml.m5.xlarge"
+    )
+    model_approval_status = ParameterString(
+        name="ModelApprovalStatus", default_value="PendingManualApproval"
+    )
+    glue_database = ParameterString(
+        name="GlueDatabase", default_value=glue_database_name
+    )
+    glue_table = ParameterString(
+        name="GlueTable", default_value=glue_table_name
+    )
+    
+    # Create a ScriptProcessor for data preprocessing with requirements.txt
+    script_processor = ScriptProcessor(
+        image_uri=sagemaker.image_uris.retrieve(
+            framework="sklearn",
             region=region,
             version="1.0-1",
             py_version="py3",
             instance_type="ml.m5.xlarge",
-        )
-    script_processor = ScriptProcessor(
-        image_uri=processing_image_uri,
+        ),
         instance_type=processing_instance_type,
         instance_count=processing_instance_count,
-        base_job_name=f"{base_job_prefix}/sklearn-abalone-preprocess",
+        base_job_name=f"{base_job_prefix}/requirements-preprocess",
         command=["python3"],
         sagemaker_session=sagemaker_session,
         role=role,
         output_kms_key=bucket_kms_id,
     )
+    
+    # Processing step using AWS Data Wrangler with requirements.txt
     step_process = ProcessingStep(
         name="PreprocessAbaloneData",
         processor=script_processor,
+        inputs=[
+            # Add requirements.txt as an input
+            ProcessingInput(
+                source=f"s3://{default_bucket}/SMUSMLOPS/requirements-preprocess/input/dependencies/",
+                destination="/opt/ml/processing/input/requirements",
+                input_name="requirements"
+            )
+        ],
         outputs=[
             ProcessingOutput(output_name="train", source="/opt/ml/processing/train"),
             ProcessingOutput(output_name="validation", source="/opt/ml/processing/validation"),
             ProcessingOutput(output_name="test", source="/opt/ml/processing/test"),
         ],
-        code="source_scripts/preprocessing/prepare_abalone_data/main.py",  # we must figure out this path to get it from step_source directory
-        job_arguments=["--input-data", f"s3://{default_bucket}/ml_pipelines/data/abalone-dataset.csv"],
+        code="source_scripts/preprocessing/prepare_abalone_data/main.py",
+        job_arguments=[
+            "--database-name", glue_database,
+            "--table-name", glue_table
+        ],
     )
 
     # training step for generating model artifacts
     model_path = f"s3://{default_bucket}/{base_job_prefix}/AbaloneTrain"
 
-    try:
-        training_image_uri = sagemaker_session.sagemaker_client.describe_image_version(ImageName=training_image_name)[
-            "ContainerImage"
-        ]
-    except (sagemaker_session.sagemaker_client.exceptions.ResourceNotFound):
-        training_image_uri = sagemaker.image_uris.retrieve(
-            framework="xgboost",
-            region=region,
-            version="1.0-1",
-            py_version="py3",
-            instance_type="ml.m5.xlarge",
-        )
+    image_uri = sagemaker.image_uris.retrieve(
+        framework="xgboost",
+        region=region,
+        version="1.0-1",
+        py_version="py3",
+        instance_type="ml.m5.xlarge",
+    )
 
     xgb_train = Estimator(
-        image_uri=training_image_uri,
+        image_uri=image_uri,
         instance_type=training_instance_type,
         instance_count=1,
         output_path=model_path,
@@ -230,7 +165,7 @@ def get_pipeline(
 
     # processing step for evaluation
     script_eval = ScriptProcessor(
-        image_uri=training_image_uri,
+        image_uri=image_uri,
         command=["python3"],
         instance_type=processing_instance_type,
         instance_count=1,
@@ -274,22 +209,9 @@ def get_pipeline(
         )
     )
 
-    try:
-        inference_image_uri = sagemaker_session.sagemaker_client.describe_image_version(ImageName=inference_image_name)[
-            "ContainerImage"
-        ]
-    except (sagemaker_session.sagemaker_client.exceptions.ResourceNotFound):
-        inference_image_uri = sagemaker.image_uris.retrieve(
-            framework="xgboost",
-            region=region,
-            version="1.0-1",
-            py_version="py3",
-            instance_type="ml.m5.xlarge",
-        )
     step_register = RegisterModel(
         name="RegisterAbaloneModel",
         estimator=xgb_train,
-        image_uri=inference_image_uri,
         model_data=step_train.properties.ModelArtifacts.S3ModelArtifacts,
         content_types=["text/csv"],
         response_types=["text/csv"],
@@ -314,7 +236,7 @@ def get_pipeline(
         else_steps=[],
     )
 
-    # pipeline instance
+    # Create pipeline
     pipeline = Pipeline(
         name=pipeline_name,
         parameters=[
@@ -322,7 +244,8 @@ def get_pipeline(
             processing_instance_count,
             training_instance_type,
             model_approval_status,
-            input_data,
+            glue_database,
+            glue_table,
         ],
         steps=[step_process, step_train, step_eval, step_cond],
         sagemaker_session=sagemaker_session,
